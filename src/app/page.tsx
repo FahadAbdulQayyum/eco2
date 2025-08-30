7b4939d796ee477030c073fed65eaa761d3cb869
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-08-30 21:43:02 +0500

    Added DressStyle components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ProductListSec from "@/components/common/ProductListSec";
 import Brands from "@/components/homepage/Brands";
+import DressStyle from "@/components/homepage/DressStyle";
 import Header from "@/components/homepage/Header";
 import Reviews from "@/components/homepage/Reviews";
 import { Product } from "@/types/product.types";
@@ -225,7 +226,9 @@ export default function Home() {
             viewAllLink="/shop#top-selling"
           />
         </div>
-        
+        <div className="mb-[50px] sm:mb-20">
+          <DressStyle />
+        </div>
         <Reviews data={reviewsData} />
       </main>
     </>
